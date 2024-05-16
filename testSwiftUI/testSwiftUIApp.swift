7c4365cecec00c2b853864aa5f17e09873fd5372
mgKhantt <khantphonenaing156@gamil.com>
2024-05-16 17:07:08 +0630

    Initial Commit

---
+++ testSwiftUI/testSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  testSwiftUIApp.swift
+//  testSwiftUI
+//
+//  Created by Khant Phone Naing  on 16/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct testSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
